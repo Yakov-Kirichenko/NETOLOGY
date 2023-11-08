cb080be3fd637603afa503c742787d8b4a973b89
Yakov-Kirichenko <148801723+Yakov-Kirichenko@users.noreply.github.com>
2023-11-08 10:32:49 +0300

    Доработка 2
    доработан код 1-го и 2-го задания

--- Spark_Netology.py
+++ Spark_Netology.py
@@ -1,14 +1,14 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col,lead,round,max
+from pyspark.sql.functions import col,lead,max
 from pyspark.sql.window import Window
 spark = SparkSession.builder.appName("COVID-19 Data Analysis").getOrCreate()
 df=(spark.read.option('header',True).option('inferSchema',True).csv('/home/owid-covid-data.csv'))
-df1=df.select(df.iso_code,df.location,(round(((df.total_cases*100) / df.population),0)).alias('perscent'))\
+df1=df.select(df.iso_code,df.location,((df.total_cases*100) / df.population).cast('decimal(12,2)').alias('perscent'))\
 .filter(df.date=='2021-03-31')\
 .orderBy(((df.total_cases*100) / df.population).desc()).limit(15)
 df1.write.csv('/home/covid-result1.csv')
 df_tr=df.select('date','location','new_cases')\
-.where(~col('location').isin('Asia','Europe','European Union','World','South America','North America'))\
+.filter(~col('iso_code').startswith('OWID'))\
 .where(col('date').between('2021-03-22','2021-03-28')).groupBy('location').agg(max('new_cases').alias('new_cases')).orderBy(col('new_cases').desc()).limit(10)
 df_result=df_tr.join(df,['location','new_cases']).select('date','location','new_cases').orderBy(col('new_cases').desc())
 df_result.write.csv('/home/covid-result2.csv')
